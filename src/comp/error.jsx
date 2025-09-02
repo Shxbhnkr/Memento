eedcb700c15a197f0e78ebef7617c78c0d5923e8
Shxbhnkr <shubh1yaduvanshi1199@gmail.com>
2025-09-02 13:59:01 +0530

    error page ui correction

--- src/comp/error.jsx
+++ src/comp/error.jsx
@@ -1,8 +1,9 @@
 export function Showerror({text}){
-    return<div className="w-full h-full relative">
-
-            <img className="absolute top-[14rem] left-[45rem]" width="90" src="https://img.icons8.com/laces/64/skull.png" alt="skull"/>
-            <div className="absolute top-[20rem] left-[10rem] font-bold m-19 font-inter text-center text-5xl text-gray-300">{text}</div>
+    return<div className="w-full h-full pt-48">
+            <div className="flex justify-center">
+            <img width="80" src="https://img.icons8.com/laces/64/skull.png" alt="skull"/>
+            </div>
+            <div className="font-bold m-19 font-inter text-center text-5xl text-gray-300">{text}</div>
         </div> ;
     
 }
