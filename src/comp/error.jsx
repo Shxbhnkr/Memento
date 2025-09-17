55bf1f1ead7b298825b55d8472f99ff8d71f7b3a
Shxbhnkr <shubhyadav.dev@gmail.com>
2025-09-18 01:29:28 +0530

    responsive 2.0

--- src/comp/error.jsx
+++ src/comp/error.jsx
@@ -3,7 +3,7 @@ export function Showerror({text}){
             <div className="flex justify-center">
             <img width="80" src="https://img.icons8.com/laces/64/skull.png" alt="skull"/>
             </div>
-            <div className="font-bold m-19 font-inter text-center text-5xl text-gray-300">{text}</div>
+            <div className="lg:font-bold font-semibold m-19 font-inter text-center lg:text-5xl text-xl text-gray-300">{text}</div>
         </div> ;
     
 }
